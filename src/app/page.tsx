60c52df57a1ced7014b044c31c80e741194d62f9
guitarbuju <gerardoamadrid@gmail.com>
2025-05-07 00:05:11 +0200

    welcome

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,29 @@
-
 import Image from "next/image";
 import logo from "@/public/surgery.jpg";
 import RegisterForm from "@/components/RegisterForm";
 
+
 export default function Home() {
   return (
-    
-     <div className="lg:flex justify-between items-center pt-1 pb-1">
-      <div className=" flex flex-col justify-center items-center align-middle sm:pl-10 gap-4">
+    <div className="lg:flex justify-between items-center pt-1 pb-1">
+      <div className=" flex flex-col justify-center items-center  sm:pl-10 gap-4">
         <div className="flex flex-col items-start">
-          <h1 className="scroll-m-20 text-4xl text-start font-bold tracking-tight text-slate-700 ">Welcome</h1>
-        <p className="scroll-m-20 text-md text-start  tracking-tight text-slate-600 ">please register your user</p>
-        <p className="scroll-m-20 text-md text-start  tracking-tight text-slate-600 ">flick the switch to enter doctors or staff members.</p>
+          <h1 className="scroll-m-20 text-4xl text-start font-bold tracking-tight text-slate-700 ">
+            Welcome
+          </h1>
+          <p className="scroll-m-20 text-md text-start  tracking-tight text-slate-600 ">
+            please register your user
+          </p>
+          <p className="scroll-m-20 text-md text-start  tracking-tight text-slate-600 ">
+            flick the switch to enter doctors or staff members.
+          </p>
         </div>
-        
-        <RegisterForm/>
+
+        <RegisterForm />
       </div>
-       
-       
-        <Image src={logo} alt="" width={1200} className="hidden lg:block"/>
-    
-     </div>
-      
-          
-    
+
+      <Image src={logo} alt="" width={1200} className="hidden lg:block" />
+     
+    </div>
   );
 }
